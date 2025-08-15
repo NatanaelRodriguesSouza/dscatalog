ef5f9005c477a4b5dc7db2cb5471226871a5e79d
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-08-15 17:19:25 -0300

    criado o controlador de categoria

---
+++ backend/src/main/java/com/example/DsCatalog/controllers/CategoryController.java
@@ -0,0 +1,21 @@
+package com.example.DsCatalog.controllers;
+
+import com.example.DsCatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1l , "Livros"));
+        return ResponseEntity.ok(list);
+    }
+}

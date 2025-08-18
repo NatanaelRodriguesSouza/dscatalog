e0b904487c0467239c18dbfebb80ae45b243f6e0
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-08-18 20:24:06 -0300

    FindAll , FindById

--- backend/src/main/java/com/example/DsCatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/example/DsCatalog/controllers/CategoryController.java
@@ -1,8 +1,12 @@
 package com.example.DsCatalog.controllers;
 
+import com.example.DsCatalog.Dto.CategoryDTO;
 import com.example.DsCatalog.entities.Category;
+import com.example.DsCatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +16,17 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
+    @Autowired
+    private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1l , "Livros"));
-        return ResponseEntity.ok(list);
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> result = service.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO>findById(@PathVariable Long id){
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- backend/src/main/java/com/example/DsCatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/DsCatalog/services/CategoryService.java
@@ -1,7 +1,32 @@
 package com.example.DsCatalog.services;
 
+import com.example.DsCatalog.Dto.CategoryDTO;
+import com.example.DsCatalog.entities.Category;
+import com.example.DsCatalog.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Transactional
+    public List<CategoryDTO> findAll(){
+        List<Category> result = categoryRepository.findAll();
+        List<CategoryDTO> list = result.stream().map(x->new CategoryDTO(x)).collect(Collectors.toList());
+        return list;
+    }
+    @Transactional
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
+    }
 }
